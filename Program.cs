9bdd4d28c79c9871e3f83c8a99c819a19fce1de8
JackapakaDan <123993428+JackapakaDan@users.noreply.github.com>
2023-03-12 17:15:35 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -23,27 +23,132 @@ public static class Program
         {
             road3_256_arr.Append(int.Parse(road3.ToString()));
         }
-        int index = 1;
-        int[] road1_256_asc = SortAsc256(road1_256_arr, index);
-        int[] road1_256_desc = SortDesc256(road1_256_arr, index);
+        //int index = 1;
+        //int[] road1_256_asc = BubbleSortAsc256(road1_256_arr);
+        //int[] road1_256_desc = BubbleSortDesc256(road1_256_arr);
 
-        Search(road1_256_asc);
-
+        //Search(road1_256_asc);
     }
-    static int[] SortAsc256(int[] arr, int index)
+    static int[] BubbleSortAsc256(int[] arr)
     {
-        int count = 0;
-        while (count < 255)
+        for (int i = 0; i < arr.Length; i++)
         {
-
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
-
-        Display10th(arr, index);
+        Display10th(arr);
         return arr;
     }
-    static int[] SortDesc256(int[] arr, int index)
+    static int[] BubbleSortDesc256(int[] arr)
     {
-        Display10th(arr, index);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j] < arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        Display10th(arr);
+        return arr;
+    }
+
+    static int[] InsertionSortAsc256(int[] arr)
+    {
+        int index = 0;
+        Stack<int> stack = new Stack<int>();
+        int[] arr1 = new int[arr.Length];
+
+        arr1.Append(arr[0]);
+        for (int i = 1; i < arr.Length; i++)
+        {
+
+            for (int j = arr1.Length;j<0; j--)
+            {
+                if (arr1[j] > arr[i])
+                {
+                    stack.Push(arr1[j]);
+                    
+                }
+                else
+                {
+                    index = j;
+                    arr1[j] = arr[i];
+                    break;
+
+
+                }
+                
+
+            }
+            for (int k = 0; k> stack.Count; k++)
+            {
+                arr1[index]=stack.ElementAt(k);
+               
+                stack.Pop();
+                index++;
+
+            }
+
+        }
+        arr=arr1;
+
+        Display10th(arr);
+        return arr;
+    }
+    static int[] InsertionSortDesc256(int[] arr)
+    {
+        int index = 0;
+        Stack<int> stack = new Stack<int>();
+        int[] arr1 = new int[arr.Length];
+
+        arr1.Append(arr[0]);
+        for (int i = 1; i < arr.Length; i++)
+        {
+
+            for (int j = arr1.Length; j < 0; j--)
+            {
+                if (arr1[j] < arr[i])
+                {
+                    stack.Push(arr1[j]);
+
+                }
+                else
+                {
+                    index = j;
+                    arr1[j] = arr[i];
+                    break;
+
+
+                }
+
+
+            }
+            for (int k = 0; k > stack.Count; k++)
+            {
+                arr1[index] = stack.ElementAt(k);
+
+                stack.Pop();
+                index++;
+
+            }
+
+        }
+        arr = arr1;
+
+        Display10th(arr);
         return arr;
     }
     static void Search(int[] arr)
@@ -85,41 +190,142 @@ public static class Program
                     $"Unexpected error:  {exception.Message}");//error handling (if the user enters a non numeric value)
             }
         }
-        /*
-        int place = 0;
-        if (arr.Length % 2 == 0)
+        Console.WriteLine("Enter the searching algorithm you wish to use 1.Linear Search, or 2.Binary Search:");
+        int algorithm = 1;
+
+        try
         {
-            place = arr.Length / 2;
+            algorithm = Convert.ToInt32(Console.ReadLine());
+            if (algorithm != 1 && algorithm != 2)
+            {
+                error = true;
+
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("ERROR...You should have entered a number!");//error handling (if the user enters a non numeric value)
+            error = true;//error handling (if the user enters a non numeric value)
+        }
+        catch (Exception exception)
+        {
+            error = true;
+            Console.WriteLine(
+                $"Unexpected error:  {exception.Message}");//error handling (if the user enters a non numeric value)
+        }
+        while (error)
+        {
+            Console.WriteLine("Enter the searching algorithm you wish to use 1.Linear Search, or 2.Binary Search:");//error handling (if the user enters a non numeric value)
+            error = false;
+            try
+            {
+                algorithm = Convert.ToInt32(Console.ReadLine());
+                if (algorithm != 1 && algorithm != 2)
+                {
+                    error = true;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR...You should have entered a number!");
+                error = true;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(
+                    $"Unexpected error:  {exception.Message}");//error handling (if the user enters a non numeric value)
+            }
+        }
+        bool itemFound=false;
+        if (algorithm==1)
+        {
+            itemFound = LinearSearch(arr, value);
 
         }
         else
         {
-            place = (arr.Length + 1) / 2;
-        }
-        while (true)
-        {
-            if (arr.Length % 2 == 0)
+            int start = 0;
+            int end = arr.Length;
+            int mid = 0; 
+            if (arr.Length%2 == 0 )
             {
-                if (arr[arr.Length / 2] == value)
-                {
-                    Console.WriteLine("The number " + value.ToString() + " was found at position " + place);
-                }
-                else if (arr[arr.Length / 2] > value)
-                {
-                    int[] half_arr = new int[arr.Length / 2];
-                    for (int i = 0; i < arr.Length; i++)
-                    {
-                        if (arr[i] > value)
-                        {
+                mid = arr[arr.Length / 2];
+            }
+            else
+            {
+                mid = (arr.Length+1) / 2;
+            }
+            itemFound= BinarySearch(arr, start, mid, end, value);
 
-                        }
-                    }
-                    Array.Resize(ref arr, arr.Length / 2);
-                }
+        }
+
+        if (itemFound)
+        {
+            Console.WriteLine("The value is in the file!");
+        }
+        else
+        {
+            Console.WriteLine("ERROR 404: ITEM NOT FOUND IN THE FILE");
+        }
+    }
+    static bool BinarySearch(int[] arr, int start, int mid, int end, int value)
+    {
+        if (mid == value)
+        {
+            return true;
+        }
+        else if(end==start && mid != value)
+        {
+            return false;
+        }
+        else if (mid > value)
+        {
+            end = mid;
+            if (end%2 == 0)
+            {
+                BinarySearch(arr, start, end/2, end, value);
+            }
+            else
+            {
+                BinarySearch(arr, start, (end+1) / 2, end, value);
+
+            }
+
+        }
+        else
+        {
+            start = mid;
+            if (start % 2 == 0)
+            {
+                BinarySearch(arr, start, start / 2, end, value);
+            }
+            else
+            {
+                BinarySearch(arr, start, (start + 1) / 2, end, value);
 
             }
         }
-        */
+        return false;
+
+
+    }
+    
+    static bool LinearSearch(int[] arr, int item)
+    {
+        bool found = false;
+        for (int i = 0; i > arr.Length; i++)
+        {
+            if(arr[i]==item)
+            {
+                found=true;
+
+            }
+
+
+
+        }
+        return found;
+
     }
     static int[] Merge256(int[] arr1, int[]arr3)
     {
@@ -131,22 +337,9 @@ public static class Program
         int[] merged_arr = new int[4096];
         return merged_arr;
     }
-    static void Display10th(int[] arr, int index)
+    static void Display10th(int[] arr)
     {
-        if (index == 1)
-        {
-            Console.WriteLine("The tenth value in the first array is " + arr[9].ToString());
+        Console.WriteLine("The tenth value in the first array is " + arr[9].ToString());
 
-        }
-        if (index == 2)
-        {
-            Console.WriteLine("The tenth value in the first array is " + arr[9].ToString());
-
-        }
-        if (index == 3)
-        {
-            Console.WriteLine("The tenth value in the first array is " + arr[9].ToString());
-
-        }
     }
 }
